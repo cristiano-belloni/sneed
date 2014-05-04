97f9a1cff8a733cc1d488de93f1d2428d908e53e
janesconference <janesconference@bitterspring.net>
2014-05-04 17:49:38 +0100

    First sneed hacky version, with play / stop keybindings and save/load.

--- sneed.js
+++ sneed.js
@@ -1,9 +1,10 @@
 define(['require',
-    'github:ajaxorg/ace-builds@master/src-min-noconflict/ace',], function(require, ace) {
+    'github:ajaxorg/ace-builds@master/src-min-noconflict/ace'
+    ], function(require, ace, chuck) {
 
     var pluginConf = {
         audioOut: 1,
-        audioIn: 1,
+        audioIn: 0,
         name: "Sneed",
         version: '0.0.1',
         hyaId: 'SNEED-PREVIOUSLY-CHUCK',
@@ -16,36 +17,66 @@ define(['require',
         }
     }
 
-    var initPlugin = function(args, resources) {
-        
-        this.name = args.name;
-        this.id = args.id;
-        this.audioSource = args.audioSources[0];
-        this.audioDestination = args.audioDestinations[0];
-        this.context = args.audioContext;
-        this.domEl = args.div;
-        this.domEl.style.height = pluginConf.ui.height+'px';
+    var initPlugin = function(initArgs) {
 
-        var editor = ace.edit(this.domEl);
-        editor.setFontSize("14px");
-        //editor.setTheme("ace/theme/mono_industrial");
-        //editor.getSession().setMode("ace/mode/javascript");
-        editor.setValue("// Ctrl-S or Cmd-S when code window is on focus executes code.\n// this.source = source node\n// this.dest = destination node\n// this.context = audio context\n// Example: this.source.connect (this.dest);");
-        editor.clearSelection();
-        
-        editor.commands.addCommand({
-            name: 'saveCommand',
-            bindKey: {win: 'Ctrl-S',  mac: 'Command-S'},
-            exec: function(editor) {
-                console.log ("Intercepted save command, editor is", editor);
-                var code = editor.getValue();
-                console.log ("And code is", code);
-            }.bind(this),
-            readOnly: true // false if this command should not apply in readOnly mode
+        require (['./chuckjs/dist/chuck-noamd'], function () {
+            console.log (arguments);
+
+            this.name = initArgs.name;
+            this.id = initArgs.id;
+            this.audioDestination = initArgs.audioDestinations[0];
+            this.context = initArgs.audioContext;
+            this.domEl = initArgs.div;
+            this.domEl.style.height = pluginConf.ui.height+'px';
+            this.chuckModule = arguments[0].chuck;
+            this.code = "";
+
+            if (initArgs.initialState && initArgs.initialState.data) {
+                /* Load data */
+                this.code = initArgs.initialState.data;
+            }
+
+            this.chuck = new this.chuckModule.Chuck(this.context, this.audioDestination);
+
+            var editor = ace.edit(this.domEl);
+            editor.setFontSize("14px");
+            //editor.setTheme("ace/theme/mono_industrial");
+            //editor.getSession().setMode("ace/mode/javascript");
+            editor.clearSelection();
+            editor.setValue (this.code, -1);
+
+            editor.commands.addCommand({
+                name: 'runCommand',
+                bindKey: {win: 'Ctrl-R',  mac: 'Command-R'},
+                exec: function(editor) {
+                    console.log ("Intercepted save command, editor is", editor);
+                    this.code = editor.getValue();
+                    console.log ("And code is", this.code);
+                    this.chuck.execute(this.code).done(function () { console.log("The program finished playing"); });
+                }.bind(this),
+                readOnly: true // false if this command should not apply in readOnly mode
+            });
+
+            editor.commands.addCommand({
+                name: 'haltCommand',
+                bindKey: {win: 'Ctrl-H',  mac: 'Command-H'},
+                exec: function(editor) {
+                    console.log ("Intercepted save command, editor is", editor);
+                    this.code = editor.getValue();
+                    console.log ("And code is", this.code);
+                    this.chuck.stop();
+                }.bind(this),
+                readOnly: true // false if this command should not apply in readOnly mode
+            });
+
+            var saveState = function () {
+                return { data: this.code };
+            };
+            initArgs.hostInterface.setSaveState (saveState.bind (this));
+
+            // Initialization made it so far: plugin is ready.
+            initArgs.hostInterface.setInstanceStatus ('ready');
         });
-
-        // Initialization made it so far: plugin is ready.
-        args.hostInterface.setInstanceStatus ('ready');
     };
         
     return {
